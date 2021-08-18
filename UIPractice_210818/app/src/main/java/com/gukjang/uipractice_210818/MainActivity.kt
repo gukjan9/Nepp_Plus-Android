876d6fae84148cf2d6183842c20025e9266dd9e8
KyeongYoon Kim <39180786+NASA-GukJang@users.noreply.github.com>
2021-08-18 16:21:03 +0900

    UIPractice

---
+++ UIPractice_210818/app/src/main/java/com/gukjang/uipractice_210818/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gukjang.uipractice_210818
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

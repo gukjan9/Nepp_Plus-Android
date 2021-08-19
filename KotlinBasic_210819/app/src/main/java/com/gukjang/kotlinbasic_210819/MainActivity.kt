b53d16faba1793f50a62467b3507768b225d2eb5
NASA-GukJang <gukjang1997@gmail.com>
2021-08-19 10:12:47 +0900

    Button Event - print Log.d

--- KotlinBasic_210819/app/src/main/java/com/gukjang/kotlinbasic_210819/MainActivity.kt
+++ KotlinBasic_210819/app/src/main/java/com/gukjang/kotlinbasic_210819/MainActivity.kt
@@ -2,6 +2,8 @@ package com.gukjang.kotlinbasic_210819
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,7 +12,9 @@ class MainActivity : AppCompatActivity() {
 
         logBtn01.setOnClickListener {
 
-
+            Log.d("메인화면", "첫 번째 로그 버튼 눌림")      // debug
+            Log.e("메인화면", "e로 로그 찍어봄")            // error
+            Log.i("메인화면", "i로 로그 찍어봄")            //
         }
     }
 }

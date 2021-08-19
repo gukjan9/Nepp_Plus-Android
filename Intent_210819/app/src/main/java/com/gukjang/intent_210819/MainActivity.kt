920f60ed0f6cf48948c4c4c71a590df20ba3ab12
NASA-GukJang <gukjang1997@gmail.com>
2021-08-19 15:17:20 +0900

    MainActivity -> OtherActivity

--- Intent_210819/app/src/main/java/com/gukjang/intent_210819/MainActivity.kt
+++ Intent_210819/app/src/main/java/com/gukjang/intent_210819/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.gukjang.intent_210819
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherBtn.setOnClickListener {                 // 버튼이 눌리면 OtherActivity 로
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }

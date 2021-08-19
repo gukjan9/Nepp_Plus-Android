704b3ef6ec760559a6b2df8bb1c25385eac59880
NASA-GukJang <gukjang1997@gmail.com>
2021-08-19 10:36:21 +0900

    Toast Button

--- KotlinBasic_210819/app/src/main/java/com/gukjang/kotlinbasic_210819/MainActivity.kt
+++ KotlinBasic_210819/app/src/main/java/com/gukjang/kotlinbasic_210819/MainActivity.kt
@@ -3,6 +3,7 @@ package com.gukjang.kotlinbasic_210819
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,18 @@ class MainActivity : AppCompatActivity() {
             Log.d("메인화면", "첫 번째 로그 버튼 눌림")      // debug
             Log.e("메인화면", "e로 로그 찍어봄")            // error
             Log.i("메인화면", "i로 로그 찍어봄")            //
+        }
+
+        logBtn02.setOnClickListener {
+            Log.d("메인화면", "두 번째 로그 버튼 눌림")
+        }
+
+        toastBtn.setOnClickListener {
+            Toast.makeText(this, "연습용 토스트 띄우기", Toast.LENGTH_SHORT).show()
+        }
+
+        toastBtn02.setOnClickListener {
+            Toast.makeText(this, "두 번째 토스트", Toast.LENGTH_SHORT).show()
         }
     }
 }

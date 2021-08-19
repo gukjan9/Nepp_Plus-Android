004efb192061c178edba9f79b1a8c466ea124d73
NASA-GukJang <gukjang1997@gmail.com>
2021-08-19 15:03:18 +0900

    OtherActivity 화면 added

---
+++ Intent_210819/app/src/main/java/com/gukjang/intent_210819/OtherActivity.kt
@@ -0,0 +1,11 @@
+package com.gukjang.intent_210819
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OtherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+    }
+}

48c3ebca1af9924295522c1f1aa8b2f579a5cfa9
NASA-GukJang <gukjang1997@gmail.com>
2021-08-19 12:00:32 +0900

    practice kotlin grammar

--- KotlinGrammar_210819/app/src/main/java/com/gukjang/kotlingrammar_210819/MainActivity.kt
+++ KotlinGrammar_210819/app/src/main/java/com/gukjang/kotlingrammar_210819/MainActivity.kt
@@ -2,6 +2,7 @@ package com.gukjang.kotlingrammar_210819
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,8 +10,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var myName : String
+        var myBirthYear : Int       // var - 변경 가능한 변수, val - 변경 불가능한 변수
+
         myName = "GukJang"
+        Log.d("변수 확인", myName)          // String만 출력
+
+        myBirthYear = 1997
+        Log.d("출생 년도", myBirthYear.toString())
 
 
+        // 조건문
+        val userAge = 18
+
+        if(userAge >= 20){
+            Log.d("나이 확인", "성인입니다.")
+        }
+        else if (userAge >= 17){
+            Log.d("나이 확인", "고등학생입니다.")
+        }
+        else{
+            Log.d("나이 확인", "나이 확인 불가")
+        }
+
+
+        // 논리연산자
+        val companyASalary = 5800
+        val companyADistance = 12.5
+        val companyABalance = true
+
+        if(companyASalary >= 4000){
+
+        }
+
+        if(companyADistance < 10 && companyASalary >= 3000){
+
+        }
+
+        if(companyABalance || companyASalary >= 5000){              // == true (x)
+
+        }
     }
 }

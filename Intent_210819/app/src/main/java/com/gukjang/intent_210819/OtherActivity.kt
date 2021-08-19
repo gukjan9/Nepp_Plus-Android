c27a74461ad59138c59150c3128f18c510f0f49d
NASA-GukJang <gukjang1997@gmail.com>
2021-08-19 15:35:04 +0900

    using finish() instead of intent to return to MainActivity

--- Intent_210819/app/src/main/java/com/gukjang/intent_210819/OtherActivity.kt
+++ Intent_210819/app/src/main/java/com/gukjang/intent_210819/OtherActivity.kt
@@ -1,11 +1,20 @@
 package com.gukjang.intent_210819
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        returnToMainBtn.setOnClickListener {
+//            val returnIntent = Intent(this, MainActivity::class.java)
+//            startActivity(returnIntent)
+
+            finish()
+        }
     }
 }

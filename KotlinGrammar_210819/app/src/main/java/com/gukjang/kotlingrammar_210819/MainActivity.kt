9a787b71cb7e4d2549c232da4896c5e8ccd9b2fc
NASA-GukJang <gukjang1997@gmail.com>
2021-08-19 10:54:18 +0900

    Kotlin 문법 연습

---
+++ KotlinGrammar_210819/app/src/main/java/com/gukjang/kotlingrammar_210819/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.gukjang.kotlingrammar_210819
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var myName : String
+        myName = "GukJang"
+
+
+    }
+}

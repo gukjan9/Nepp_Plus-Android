5549a3aae38b0499629f9cf16e1fba48ce6e3a9e
NASA-GukJang <gukjang1997@gmail.com>
2021-08-19 12:17:53 +0900

    입력한 텍스트를 toast 메세지로

--- KotlinUIPractice_210819/app/src/main/java/com/gukjang/kotlinuipractice_210819/MainActivity.kt
+++ KotlinUIPractice_210819/app/src/main/java/com/gukjang/kotlinuipractice_210819/MainActivity.kt
@@ -2,10 +2,23 @@ package com.gukjang.kotlinuipractice_210819
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToTextViewBtn.setOnClickListener {              // 입력한 메세지를 변수에 저장해서 텍스트뷰의 text 속성으로 대입
+            val inputMessage = messageEdt.text.toString()
+            resultTxt.text = inputMessage
+        }
+
+        toastBtn.setOnClickListener {
+            val inputMessage = messageEdt.text.toString()
+
+            Toast.makeText(this, inputMessage, Toast.LENGTH_SHORT).show()
+        }
     }
 }

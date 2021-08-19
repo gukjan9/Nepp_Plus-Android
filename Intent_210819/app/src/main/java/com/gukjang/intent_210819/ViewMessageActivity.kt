9804d20bc4fa410eab8e8aac575aa86c9823f3ff
NASA-GukJang <gukjang1997@gmail.com>
2021-08-19 15:53:52 +0900

    received data from Activity and Display

--- Intent_210819/app/src/main/java/com/gukjang/intent_210819/ViewMessageActivity.kt
+++ Intent_210819/app/src/main/java/com/gukjang/intent_210819/ViewMessageActivity.kt
@@ -2,10 +2,14 @@ package com.gukjang.intent_210819
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_message.*
 
 class ViewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_message)
+
+        val receivedMsg = intent.getStringExtra("message")
+        msgTxt.text = receivedMsg
     }
 }

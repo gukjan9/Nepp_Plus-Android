2d4d999c473cf681bdd51bd306a15dee3306fd29
NASA-GukJang <gukjang1997@gmail.com>
2021-08-23 09:57:21 +0900

    teacher's code

---
+++ Practices/(Compare)JickbangCopy_20210820-master/(Compare)JickbangCopy_20210820-master/app/src/main/java/com/neppplus/jickbangcopy_20210820/datas/RoomData.kt
@@ -0,0 +1,11 @@
+package com.neppplus.jickbangcopy_20210820.datas
+
+class RoomData(
+    val price : Int,
+    val address : String,
+    val floor : Int,
+    val description : String ) {
+
+//    추가 기능 (함수) 구현
+
+}

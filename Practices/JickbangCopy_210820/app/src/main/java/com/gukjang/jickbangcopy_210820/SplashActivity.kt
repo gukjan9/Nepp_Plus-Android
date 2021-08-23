1a5114d7095061e4bc0e0b6e62c775162bf9e3dc
NASA-GukJang <gukjang1997@gmail.com>
2021-08-23 12:36:07 +0900

    handler, intent postDelayed

--- Practices/JickbangCopy_210820/app/src/main/java/com/gukjang/jickbangcopy_210820/SplashActivity.kt
+++ Practices/JickbangCopy_210820/app/src/main/java/com/gukjang/jickbangcopy_210820/SplashActivity.kt
@@ -1,11 +1,26 @@
 package com.gukjang.jickbangcopy_210820
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        // Intent 관련 코드를 3초 후에 실행
+
+        val myHandler = Handler(Looper.getMainLooper())         // 안드로이드 os가 주는 Looper 사용해야함
+
+       myHandler.postDelayed({
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+            finish()
+        }, 3000)
+
+
     }
 }

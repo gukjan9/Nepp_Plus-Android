c2464dea8ea7d6ff185ce1d1e07392c7cead35e6
NASA-GukJang <gukjang1997@gmail.com>
2021-08-24 16:11:32 +0900

    Library 추가 - circleImageView, photoView

--- LibraryPractice_210824/app/src/main/java/com/gukjang/librarypractice_210824/MainActivity.kt
+++ LibraryPractice_210824/app/src/main/java/com/gukjang/librarypractice_210824/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.gukjang.librarypractice_210824
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,6 +17,12 @@ class MainActivity : BaseActivity() {
     override fun setupEvents(){
         profileImg.setOnClickListener{          // 프로필 사진 클릭하면 사진 크게 보는 화면으로 이동 event 발생
             val myIntent = Intent(mContext, ViewPhotoActivity::class.java)
+            startActivity(myIntent)
+        }
+
+        callBtn.setOnClickListener{
+            val myUri = Uri.parse("tel : 02-5555-8888")
+            val myIntent = Intent(Intent.ACTION_CALL, myUri)
             startActivity(myIntent)
         }
     }

150fc44be7fd939dce54bdc73ed6a636c2540c18
NASA-GukJang <gukjang1997@gmail.com>
2021-08-24 11:42:34 +0900

    viewpager adapter 와 viewpager 연결 작업

--- ViewPager_210824/app/src/main/java/com/gukjang/viewpager_210824/MainActivity.kt
+++ ViewPager_210824/app/src/main/java/com/gukjang/viewpager_210824/MainActivity.kt
@@ -2,10 +2,18 @@ package com.gukjang.viewpager_210824
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gukjang.viewpager_210824.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mvpa : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mvpa = MainViewPagerAdapter( supportFragmentManager )
+        mainViewPager.adapter = mvpa
     }
 }

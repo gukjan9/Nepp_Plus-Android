90490c0765d10e616f68a1688e7a797e9473e7df
NASA-GukJang <gukjang1997@gmail.com>
2021-08-25 17:45:29 +0900

    variables

---
+++ JAVA/Variable_210825/src/codes/MainDrive.java
@@ -0,0 +1,12 @@
+package codes;
+
+public class MainDrive {
+	public static void main(String[] args) {
+		int myAge = 25;
+		String myName = "국장";
+		double myHeight = 165;
+		boolean isMale = false;
+		
+		System.out.println(myAge);
+	}
+}

7228b570c32fd5904830012ca03a968a583fd735
NASA-GukJang <gukjang1997@gmail.com>
2021-08-25 12:57:45 +0900

    어플 시작점을 SplashActivity로 변경 - Manifest.xml

---
+++ LibraryPractice_210824/app/src/main/java/com/gukjang/librarypractice_210824/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.gukjang.librarypractice_210824
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}

aa2d432f885d9beddf4417b786f93bee00a0ff45
NASA-GukJang <gukjang1997@gmail.com>
2021-08-25 17:03:18 +0900

    comment

--- JAVA/HelloWord_210825/src/codes/MainDrive.java
+++ JAVA/HelloWord_210825/src/codes/MainDrive.java
@@ -1,5 +1,10 @@
 package codes;
 
 public class MainDrive {
-
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
 }
+
+// Ctrl + F11 - 프로그램 실행
+// Ctrl + Space - main, syso

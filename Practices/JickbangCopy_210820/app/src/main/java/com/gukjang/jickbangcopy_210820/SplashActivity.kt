4cee05f16d55d15b067e9784bf4a88940bedaf6d
NASA-GukJang <gukjang1997@gmail.com>
2021-08-25 18:09:11 +0900

    Loading - 3 sec SplashActivity

---
+++ Practices/JickbangCopy_210820/app/src/main/java/com/gukjang/jickbangcopy_210820/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.gukjang.jickbangcopy_210820
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // 로딩 화면 3초 노출 후에 메인화면으로 이동
+        // Intent 관련 코드를 3초 후에 실행
+
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+            finish()
+        }, 3000)
+    }
+}

c286bab3f9406a9e3b42328be0003dc2c01ecc63
NASA-GukJang <gukjang1997@gmail.com>
2021-08-27 01:26:28 +0900

    Main, TabLayout, ViewPager, Fragment 연동

--- PizzaOrderApp_210825/app/src/main/java/com/gukjang/pizzaorderapp_210825/BaseActivity.kt
+++ PizzaOrderApp_210825/app/src/main/java/com/gukjang/pizzaorderapp_210825/BaseActivity.kt
@@ -2,6 +2,8 @@ package com.gukjang.pizzaorderapp_210825
 
 import androidx.appcompat.app.AppCompatActivity
 
+// 1. abstract 함수가 있으면 class 도 abstract
+// 2. 상속 받기 위해선 open / abstract
 abstract class BaseActivity : AppCompatActivity() {
     val mContext = this
 

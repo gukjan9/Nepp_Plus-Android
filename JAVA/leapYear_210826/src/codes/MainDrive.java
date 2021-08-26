1d44e68b845230da78f1ac84e761ff647b1e49c0
NASA-GukJang <gukjang1997@gmail.com>
2021-08-26 16:52:25 +0900

    LeapYear - 두 번째 방법

--- JAVA/leapYear_210826/src/codes/MainDrive.java
+++ JAVA/leapYear_210826/src/codes/MainDrive.java
@@ -9,6 +9,8 @@ public class MainDrive {
 		System.out.println("Enter year : ");
 		int year = scanner.nextInt();
 		
+		// 풀이 1. if 중첩
+		
 		if(year % 4 == 0) {
 			if(year % 100 == 0) {
 				if(year % 400 == 0) System.out.println("윤년입니다.");
@@ -17,5 +19,8 @@ public class MainDrive {
 			else System.out.println("윤년입니다.");
 		}
 		else System.out.println("윤년이 아닙니다.");
+		
+		
+		// 풀이 2. 400 -> 100 -> 4 순으로 풀기
 	}
 }

5ba5d6e5b1794a8ffc0fc352c27a0d446b75b7da
NASA-GukJang <gukjang1997@gmail.com>
2021-08-26 10:49:12 +0900

    operator - birthyear

---
+++ JAVA/Operator_practice/src/codes/MainDrive.java
@@ -0,0 +1,11 @@
+package codes;
+
+public class MainDrive {
+	public static void main(String[] args) {
+		int myBirthYear = 1997;
+		int thisYear = 2021;
+		int myKoreanAge = thisYear - myBirthYear + 1;
+		
+		System.out.println(myBirthYear + "년생-" + myKoreanAge + "세");
+	}
+}

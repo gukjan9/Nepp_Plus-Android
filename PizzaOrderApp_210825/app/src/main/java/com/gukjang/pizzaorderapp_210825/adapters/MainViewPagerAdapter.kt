73e1d060455e5826056441f3134cf456c06c3e16
NASA-GukJang <gukjang1997@gmail.com>
2021-08-27 01:07:01 +0900

    Main, TabLayout, ViewPager, Fragment 연동

--- PizzaOrderApp_210825/app/src/main/java/com/gukjang/pizzaorderapp_210825/adapters/MainViewPagerAdapter.kt
+++ PizzaOrderApp_210825/app/src/main/java/com/gukjang/pizzaorderapp_210825/adapters/MainViewPagerAdapter.kt
@@ -6,10 +6,12 @@ import androidx.fragment.app.FragmentPagerAdapter
 import com.gukjang.pizzaorderapp_210825.fragments.MyProfileFragment
 import com.gukjang.pizzaorderapp_210825.fragments.PizzaStoreListFragment
 
-class MainViewPagerAdapter (fm : FragmentManager) : FragmentPagerAdapter(fm) {
+class MainViewPagerAdapter (fm : FragmentManager) : FragmentPagerAdapter(fm) {              // fm - 변수 이름
 
+    // abstract 아님 - 부모가 하라는대로
+    // tableLayout 제목 설정 함수
     override fun getPageTitle(position: Int): CharSequence? {
-        // return super.getPageTitle(position) - 삭제하는 이유 : 결과가 정해져 있기 때문
+        // return super.getPageTitle(position) - 의미 : 부모 클래스에 있는 super.- 함수 실행해라 / 삭제하는 이유 : 그대로 리턴시 override 의미 x
         return when(position){
             0 -> "피자 주문"
             else -> "내 정보 설정"
@@ -22,7 +24,7 @@ class MainViewPagerAdapter (fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
         return when(position){
-            0 -> PizzaStoreListFragment()
+            0 -> PizzaStoreListFragment()               // 객체화 ()
             else -> MyProfileFragment()
         }
     }
--- PizzaOrderApp_210825/app/src/main/java/com/gukjang/pizzaorderapp_210825/fragments/PizzaStoreListFragment.kt
+++ PizzaOrderApp_210825/app/src/main/java/com/gukjang/pizzaorderapp_210825/fragments/PizzaStoreListFragment.kt
@@ -12,12 +12,13 @@ import com.gukjang.pizzaorderapp_210825.adapters.PizzaStoreAdapter
 import com.gukjang.pizzaorderapp_210825.datas.StoreData
 import kotlinx.android.synthetic.main.fragment_pizza_store_list.*
 
-class PizzaStoreListFragment : Fragment() {
+class PizzaStoreListFragment : Fragment() {                     // fragment 는 fragment 상속
 
     val mPizzaStores = ArrayList<StoreData>()
 
     lateinit var mPizzaStoreAdapter : PizzaStoreAdapter
 
+    // onCreateView - xml 과 kt 연결하는 함수 (fragment 관련끼리)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,6 +27,7 @@ class PizzaStoreListFragment : Fragment() {
         return inflater.inflate( R.layout.fragment_pizza_store_list, container, false )
     }
 
+    // onActivityCreated - 동작 관련
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 

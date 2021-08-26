4b12b797b94a5bb9c95a1c578bb0096392b34e89
NASA-GukJang <gukjang1997@gmail.com>
2021-08-26 11:33:38 +0900

    variable practice - calc profit

--- JAVA/Operator_practice/src/codes/MainDrive.java
+++ JAVA/Operator_practice/src/codes/MainDrive.java
@@ -7,5 +7,11 @@ public class MainDrive {
 		int myKoreanAge = thisYear - myBirthYear + 1;
 		
 		System.out.println(myBirthYear + "년생-" + myKoreanAge + "세");
+		
+		double startMoney = 100000;
+		double nowMoney = 115000;
+		double profit = (nowMoney / startMoney) * 100;
+		
+		System.out.println("Profit is : " + profit + "%");
 	}
 }

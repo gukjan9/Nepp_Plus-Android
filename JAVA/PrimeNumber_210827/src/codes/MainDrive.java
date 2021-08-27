0e7936e6cc56b35a1d798dd46add05fc85673e0b
KyeongYoon Kim <39180786+NASA-GukJang@users.noreply.github.com>
2021-08-27 17:05:39 +0900

    PrimeNumber

--- JAVA/PrimeNumber_210827/src/codes/MainDrive.java
+++ JAVA/PrimeNumber_210827/src/codes/MainDrive.java
@@ -1,7 +1,26 @@
 package codes;
 
+import java.util.Scanner;
+
 public class MainDrive {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		
+		System.out.println("정수를 입력하시오 : ");
+		int num = scanner.nextInt();
+		
+		boolean isPrimeNum = true;
+		int divideNum;
+		
+		for(int i = 2; i < num / 2; i++) {				// primeNum 의 반이 넘어가면 어차피 의미가 없음 * 2
+			if(num % i == 0) {
+				isPrimeNum = false;
+				divideNum = i;
+				break;
+			}
+		}
+		
+		if(isPrimeNum) System.out.println("소수 맞음");
+		else System.out.println("소수 아님");
 	}
 }

89cc44f57dec0afab52ebc9c4760231820b91322
NASA-GukJang <gukjang1997@gmail.com>
2021-09-02 17:09:53 +0900

    main -> editPhoneNum 으로 이동 Intent

---
+++ PhoneBook_210902/app/src/main/java/com/gukjang/phonebook_210902/EditPhoneNumActivity.kt
@@ -0,0 +1,21 @@
+package com.gukjang.phonebook_210902
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditPhoneNumActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_phone_num)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setValues() {
+        TODO("Not yet implemented")
+    }
+}
--- PhoneBook_210902/app/src/main/java/com/gukjang/phonebook_210902/MainActivity.kt
+++ PhoneBook_210902/app/src/main/java/com/gukjang/phonebook_210902/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.gukjang.phonebook_210902
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    // 1. 화면에 들어오면 -> 파일에 저장된 "이름,폰번,1997-04-14" 문장 불러내기
+    // 안드로이드에서 파일 다루는법
+
+    // 2. 불러낸 데이터들을 ListView 에 뿌려준다.
+
+   override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupEvents()
@@ -11,7 +20,17 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
-        TODO("Not yet implemented")
+//        addPhoneNumBtn.setOnClickListener(object : View.OnClickListener{
+//            override fun onClick(v: View?) {
+//                TODO("Not yet implemented")
+//            }
+
+        // JAVA의 interface 를 개량한 버전의 코드 (위 참조)
+        // EditPhoneNumActivity 로 이동 : intent
+        addPhoneNumBtn.setOnClickListener{
+            val myIntent = Intent(mContext, EditPhoneNumActivity::class.java)
+            startActivity(myIntent)         // (= super.startActivity)
+        }
     }
 
     override fun setValues() {

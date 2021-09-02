ce51f17fa8d173bf35c02f5b4e4bafcd1c49e649
NASA-GukJang <gukjang1997@gmail.com>
2021-09-02 16:48:40 +0900

    BaseActivity 추가 -> MainActivity가 상속 받아서 사용

---
+++ PhoneBook_210902/app/src/main/java/com/gukjang/phonebook_210902/BaseActivity.kt
@@ -0,0 +1,17 @@
+package com.gukjang.phonebook_210902
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+    lateinit var mContext : Context
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mContext = this
+    }
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+}
---
+++ PhoneBook_210902/app/src/main/java/com/gukjang/phonebook_210902/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.gukjang.phonebook_210902
+
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setValues() {
+        TODO("Not yet implemented")
+    }
+}

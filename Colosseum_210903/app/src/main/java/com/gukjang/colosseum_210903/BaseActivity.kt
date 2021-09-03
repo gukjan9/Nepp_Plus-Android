e5fb39710110051eca014a55f32d1528fc590209
NASA-GukJang <gukjang1997@gmail.com>
2021-09-03 15:36:22 +0900

    okHttp, Internet, http (평문 통신) 허용

---
+++ Colosseum_210903/app/src/main/java/com/gukjang/colosseum_210903/BaseActivity.kt
@@ -0,0 +1,18 @@
+package com.gukjang.colosseum_210903
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+    lateinit var mContext : Context
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mContext = this
+    }
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+}

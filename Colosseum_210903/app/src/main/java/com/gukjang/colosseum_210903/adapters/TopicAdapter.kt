192b32f508c8db5dcd3f09a55f53d2e8308ae44c
NASA-GukJang <gukjang1997@gmail.com>
2021-09-06 15:53:34 +0900

    Adapter GetView 수정

--- Colosseum_210903/app/src/main/java/com/gukjang/colosseum_210903/adapters/TopicAdapter.kt
+++ Colosseum_210903/app/src/main/java/com/gukjang/colosseum_210903/adapters/TopicAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.gukjang.colosseum_210903.R
 import com.gukjang.colosseum_210903.datas.TopicData
 
@@ -19,6 +22,14 @@ class TopicAdapter(
         var row = convertView
         if(row == null) row = mInflater.inflate(R.layout.topic_list_item, null)
         row!!
+
+        val data = mList[position]
+        val topicImg = row.findViewById<ImageView>(R.id.topicImg)
+        val topicTitleTxt = row.findViewById<TextView>(R.id.topicTitleTxt)
+
+        topicTitleTxt.text = data.title
+
+        Glide.with(mContext).load(data.imageURL).into(topicImg)
 
         return row
     }

f756fe1c67bbf4b34d7d263dea8c70b0903e676e
NASA-GukJang <gukjang1997@gmail.com>
2021-09-07 16:51:44 +0900

    supportActionBar 가 존재할때만 custom Actionbar 실행
    변수?.let { }

--- Colosseum_210903/app/src/main/java/com/gukjang/colosseum_210903/BaseActivity.kt
+++ Colosseum_210903/app/src/main/java/com/gukjang/colosseum_210903/BaseActivity.kt
@@ -1,7 +1,9 @@
 package com.gukjang.colosseum_210903
 
+import android.app.ActionBar
 import android.content.Context
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
@@ -11,8 +13,28 @@ abstract class BaseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         mContext = this
+
+        // 액션바가 존재하는 화면에서만 실행
+        // 액션바가 null 이 아닐 때만 실행시켜줄 코드
+        supportActionBar?.let{
+            setCustomActionBar()
+        }
     }
 
     abstract fun setupEvents()
     abstract fun setValues()
+
+    // actionbar 를 커스텀 액션바로 바꿔주는 함수
+    fun setCustomActionBar(){
+        // 기본 액션바를 불러오자
+        val defaultActionBar = supportActionBar!!
+
+        // 커스텀 모드로 변경
+        defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        defaultActionBar.setCustomView(R.layout.my_custom_action_bar)
+
+        // 양 옆 여백 제거
+        val myToolbar = defaultActionBar.customView.parent as Toolbar
+        myToolbar.setContentInsetsAbsolute(0,0)
+    }
 }
--- Colosseum_210903/app/src/main/java/com/gukjang/colosseum_210903/MainActivity.kt
+++ Colosseum_210903/app/src/main/java/com/gukjang/colosseum_210903/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setCustomActionBar()
         setupEvents()
         setValues()
     }

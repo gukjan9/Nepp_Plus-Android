5a9e857661a111fbbd590b5152ab22cab73e5655
NASA-GukJang <gukjang1997@gmail.com>
2021-09-07 14:58:43 +0900

    view 의 속성인 tag 속성 이용

--- Colosseum_210903/app/src/main/java/com/gukjang/colosseum_210903/ViewTopicDetailActivity.kt
+++ Colosseum_210903/app/src/main/java/com/gukjang/colosseum_210903/ViewTopicDetailActivity.kt
@@ -1,6 +1,8 @@
 package com.gukjang.colosseum_210903
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.bumptech.glide.Glide
 import com.gukjang.colosseum_210903.adapters.ReplyAdapter
 import com.gukjang.colosseum_210903.datas.ReplyData
@@ -23,11 +25,34 @@ class ViewTopicDetailActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        // 첫번째 진영, 두번째 진영 투표버튼의 이벤트
+        val ocl = object : View.OnClickListener{
+            override fun onClick(view: View?) {
+                // 버튼이 눌리면 할 일
+                // view -> 눌린게 어떤 버튼인지? 눌린 버튼을 담아준다.
+                val clickedSideId = view!!.tag.toString().toInt()
 
+                Log.d("투표 진영 id", clickedSideId.toString())
+
+                ServerUtil.postRequestTopicVote(mContext, clickedSideId, object : ServerUtil.JsonResponseHandler{
+                    override fun onResponse(jsonObj: JSONObject) {
+                        // 투표 결과 확인
+
+                    }
+
+                })
+            }
+        }
+        voteToFirstSideBtn.setOnClickListener(ocl)
+        voteToSecondSideBtn.setOnClickListener(ocl)
     }
 
     override fun setValues() {
         mTopicData = intent.getSerializableExtra("topic") as TopicData
+
+        // 투표 버튼에 각 진영이 어떤 진영인지 버튼에 메모해두면 투표할 때 그 진영이 뭔지 알아낼 수 있다.
+        voteToFirstSideBtn.tag = mTopicData.sideList[0].id
+        voteToSecondSideBtn.tag = mTopicData.sideList[1].id
 
         Glide.with(mContext).load(mTopicData.imageURL).into(topicImg)
         titleTxt.text = mTopicData.title

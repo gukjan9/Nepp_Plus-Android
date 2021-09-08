43effa9cb899d5b76bd6543d43b0379ecec2e26e
NASA-GukJang <gukjang1997@gmail.com>
2021-09-08 11:42:18 +0900

    Push 알림을 Toast 에 띄우기

--- Colosseum_210903/app/src/main/java/com/gukjang/colosseum_210903/fcm/MyFCMService.kt
+++ Colosseum_210903/app/src/main/java/com/gukjang/colosseum_210903/fcm/MyFCMService.kt
@@ -1,6 +1,9 @@
 package com.gukjang.colosseum_210903.fcm
 
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -9,5 +12,14 @@ class MyFCMService : FirebaseMessagingService() {
         super.onMessageReceived(p0)
 
         Log.d("푸시알림", "수신이벤트")
+
+        // 토스트로 받은 내용 출력
+        val title = p0.notification!!.title
+
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.post {
+            // runOnUiThread 와 같은 역할
+            Toast.makeText(applicationContext, title,Toast.LENGTH_SHORT).show()
+        }
     }
 }

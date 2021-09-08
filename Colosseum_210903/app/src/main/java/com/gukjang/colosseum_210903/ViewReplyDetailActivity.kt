76791d5853f546908ffda2223334cded10aa6e07
NASA-GukJang <gukjang1997@gmail.com>
2021-09-08 16:55:18 +0900

    답글 달기 기능

--- Colosseum_210903/app/src/main/java/com/gukjang/colosseum_210903/ViewReplyDetailActivity.kt
+++ Colosseum_210903/app/src/main/java/com/gukjang/colosseum_210903/ViewReplyDetailActivity.kt
@@ -2,9 +2,12 @@ package com.gukjang.colosseum_210903
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.gukjang.colosseum_210903.datas.ReplyData
+import com.gukjang.colosseum_210903.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_view_reply_detail.*
 import kotlinx.android.synthetic.main.reply_list_item.*
+import org.json.JSONObject
 
 class ViewReplyDetailActivity : BaseActivity() {
     lateinit var  mReplyData : ReplyData
@@ -27,5 +30,23 @@ class ViewReplyDetailActivity : BaseActivity() {
         sideAndNicknameTxt.text = "(${mReplyData.selectedSide.title}) ${mReplyData.writer.nickname}"
 
         replyContentTxt.text = mReplyData.content
+
+        okBtn.setOnClickListener {
+            val inputContent = contentEdt.text.toString()
+
+            if(inputContent.length < 5){
+                Toast.makeText(mContext, "5자 이상 입력해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            ServerUtil.postRequestChildReply(mContext, inputContent, mReplyData.id, object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+                    runOnUiThread{
+                        contentEdt.setText("")
+                    }
+                }
+
+            })
+        }
     }
 }

638fe224dd4e079afa17d1276d30673156d41a8a
NASA-GukJang <gukjang1997@gmail.com>
2021-09-14 11:10:05 +0900

    지도 보기 화면에서 받아낸 약속 갖고 카메라 이동, 마커 찍어주기

--- MyFinalProject_210910/app/src/main/java/com/gukjang/myfinalproject_210910/ViewMapActivity.kt
+++ MyFinalProject_210910/app/src/main/java/com/gukjang/myfinalproject_210910/ViewMapActivity.kt
@@ -3,6 +3,10 @@ package com.gukjang.myfinalproject_210910
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gukjang.myfinalproject_210910.datas.AppointmentData
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.overlay.Marker
 
 class ViewMapActivity : BaseActivity() {
     lateinit var mAppointmentData : AppointmentData
@@ -20,5 +24,23 @@ class ViewMapActivity : BaseActivity() {
 
     override fun setValues() {
         mAppointmentData = intent.getSerializableExtra("appointment") as AppointmentData        // 위에서 AppointmentData 로 받기로 했음
-    }
+
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.naverMapFrag) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.naverMapFrag, it).commit()
+            }
+
+        mapFragment.getMapAsync {
+            val appointmentLatLng = LatLng(mAppointmentData.latitude, mAppointmentData.longitude)
+
+            // 약속장소로 카메라 이동
+            val cameraUpdate = CameraUpdate.scrollTo(appointmentLatLng)
+            it.moveCamera(cameraUpdate)
+
+            val marker = Marker()
+            marker.position = appointmentLatLng
+            marker.map = it
+        }
+   }
 }
--- MyFinalProject_210910/app/src/main/java/com/gukjang/myfinalproject_210910/datas/UserData.kt
+++ MyFinalProject_210910/app/src/main/java/com/gukjang/myfinalproject_210910/datas/UserData.kt
@@ -1,12 +1,13 @@
 package com.gukjang.myfinalproject_210910.datas
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 class UserData(
     var id : Int,
     var provider : String,
     @SerializedName("nick_name")
     var nickName : String,
-    var email : String) {
+    var email : String) : Serializable {
 
 }
